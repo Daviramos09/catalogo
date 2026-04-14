b2fdf27500ccc4a0da1d233e6392e79e45458ddf
Daviramos09 <davi0909ramos@gmail.com>
2026-04-14 16:55:40 -0300

    first commit

---
+++ src/components/ProdutoChild.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps(['nome' , 'preco' , 'categotia'])
+</script>
+<template>
+<li>
+    {{ produto.nome }} - R$ {{ produto.preco }} {{ categoria }}
+</li>
+</template>
+
+<style scoped>
+</style>
